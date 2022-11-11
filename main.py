367f7d1d13691c8cdfc67c30ded6bc96a751fc5a
4ce <88108711+4cecoder@users.noreply.github.com>
2022-11-11 02:03:45 -0500

    Update main.py
    better quit

--- main.py
+++ main.py
@@ -35,38 +35,39 @@ def setup_model():
     return happy_wp
 
 
-def predict(user_input, predictor):
-    max_predictions = 20
-
+def predict(user_input, predictor, max_predictions):
     result = predictor.predict_mask(user_input + " [MASK]", top_k=max_predictions)
     return result
 
 
+def check_exit(user_input):
+    if user_input == "q":
+        print("Exiting program...")
+        exit()
+
+
 # main function
 def main():
-    prediction_variant = 1
+    prediction_variant = 0
+    max_predictions = 20
     detect()
     predictor = setup_model()
 
     print("Enter sentence with a blank space where you want to predict the next word: ")
     user_input = get_input()
+    check_exit(user_input)
 
     while True:
-        new_input = predict(user_input, predictor)
+        new_input = predict(user_input, predictor, max_predictions)
         # if new_input[].token is not greater than 1 character long, then add 1 to predictionVariant
-        if len(new_input[prediction_variant].token) == 1:
+        if len(new_input[prediction_variant].token) == 1 and prediction_variant < max_predictions:
             prediction_variant += 1
         if len(new_input[prediction_variant].token) > 1:
             user_input = user_input + " " + new_input[prediction_variant].token
-
-        print(user_input)
-        next_words = get_input()
-        user_input = user_input + " " + next_words
-
-        # if user presses q, exit the program
-        if next_words == "q":
-            print("Exiting program...")
-            break
+            print(user_input)
+            next_words = get_input()
+            user_input = user_input + " " + next_words
+            check_exit(next_words)
 
 
 # call main function
